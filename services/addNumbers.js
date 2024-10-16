4a034d21c28918d39f2bdfe1e891fbcb7e22dbec
arulkumaranm-vuram <arulkumaranm@workhall.com>
2024-10-16 11:33:18 +0530

    new commit for conflict

--- services/addNumbers.js
+++ services/addNumbers.js
@@ -1,6 +1,6 @@
 const addNumbers = (valuesArray) => {
   console.log('Sample add Number Functions');
-  let totalValue = 0;
+  let totalValue = 10;
   valuesArray.forEach((eachValue) => {
     totalValue += eachValue
   })
