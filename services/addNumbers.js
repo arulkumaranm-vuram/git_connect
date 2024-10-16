a9ca56a1484280a2a55a5fb974ff860e49cd52ef
arulkumaranm-vuram <arulkumaranm@workhall.com>
2024-10-16 11:41:12 +0530

    my totalValue reset to 0

--- services/addNumbers.js
+++ services/addNumbers.js
@@ -1,6 +1,6 @@
 const addNumbers = (valuesArray) => {
   console.log('Sample add Number Functions');
-  let totalValue = 200;
+  let totalValue = 0;
   valuesArray.map((eachValue) => {
     totalValue += eachValue
   })
